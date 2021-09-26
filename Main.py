8e298830ce984117bdbbc69160b388a94e415378
Playbird12 <83607406+Jonathan-Tao@users.noreply.github.com>
2021-09-26 17:23:50 -0500

    Program Crashed

---
+++ Main.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Controller
+import time
+import keyboard
+
+x = True
+
+keyboard = Controller()
+
+while x == True:
+
+    time.sleep(1)
+    for char in "sin":
+        keyboard.press(char)
+        keyboard.release(char)
+        
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)sin
+    sin
+    sin
+    sin
+   sinsin
+   sin
+
+
+sin
+sin
+sin
