f2ca326326df08d907ca843fd728a7a880b05dcd
Playbird12 <83607406+Jonathan-Tao@users.noreply.github.com>
2022-05-31 19:02:21 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-from pynput.keyboard import Key, Controller
+#from pynput.keyboard import Key, Controller
 import time
 import keyboard
+
+
 
 x = True
 
@@ -9,13 +11,15 @@ keyboard = Controller()
 keyboard.press(Key.space)
 keyboard.release(Key.space)
 
+
+
 #Word Typing Script
 #while x == True:
 
-#    time.sleep(1)
-#    for char in "sin":
-#        keyboard.press(char)
-#        keyboard.release(char)
-#        
-#    keyboard.press(Key.enter)
-#    keyboard.release(Key.enter)
+time.sleep(1)
+for char in "@Vibin":
+    keyboard.press(char)
+    keyboard.release(char)
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
